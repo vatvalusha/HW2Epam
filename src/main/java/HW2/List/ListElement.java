f8885bac3efc8325e762111592b7bb280d2ba41d
vatvalusha <vatvalusha@gmail.com>
2017-02-15 23:35:47 +0200

    new tests

---
+++ src/main/java/HW2/List/ListElement.java
@@ -0,0 +1,7 @@
+package HW2.List;
+
+/**
+ * Created by vatva on 14.02.2017.
+ */
+public class ListElement {
+}
---
+++ src/test/java/HW2/PC/CPUTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by vatva on 15.02.2017.
+ */
+public class CPUTest {
+
+}
